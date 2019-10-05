ca3a48cddc98e8617f138f0e2237e73a4cd08973
Unknown <razeltif@gmail.com>
2019-10-05 22:50:21 +0200

    ça va péter !

---
+++ LudumDare45/Assets/PositionSouris.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionSouris : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+        Debug.Log(position);
+
+    }
+}
