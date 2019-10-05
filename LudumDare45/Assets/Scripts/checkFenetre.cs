eeb66b67b9ecd946366b6214302845e9b4e5a112
Unknown <razeltif@gmail.com>
2019-10-06 01:03:48 +0200

    [BITE] on va voir si ça fonctionne

---
+++ LudumDare45/Assets/Scripts/checkFenetre.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkFenetre : MonoBehaviour
+{
+
+    public Vector3 range;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 StartPoint = Camera.main.ScreenToWorldPoint(Vector3.zero);
+        Vector3 EndPoint = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        range = EndPoint - StartPoint;
+
+        Debug.Log(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 20f)));
+        print(range);
+    }
+}
