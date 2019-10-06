d2170d00bd130870063105c6998df1afda125758
Hugo <bombix34@gmail.com>
2019-10-06 21:40:39 +0200

    [ADD] sounds

---
+++ LudumDare45/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public AudioSource[] audiosources;
+
+    public SoundDatabase baseSounds;
+
+    void Start()
+    {
+        for (int i = 0; i < audiosources.Length; i++)
+        {
+            audiosources[i].loop = false;
+        }
+    }
+
+    public void PlaySound(int sound)
+    {
+        GetAudioSourceAvailable(baseSounds.audioDatabase[sound]);
+    }
+
+    public void GetAudioSourceAvailable(AudioClip clip)
+    {
+        for (int i = 0; i < audiosources.Length; i++)
+        {
+            if (!audiosources[i].isPlaying)
+            {
+                audiosources[i].loop = false;
+                audiosources[i].clip = clip;
+                audiosources[i].Play();
+                return;
+            }
+        }
+    }
+}
